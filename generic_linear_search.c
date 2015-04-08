5ed215149309e4445cb8209848de1bf47885e99a
DineshRai <drai89@gmail.com>
2015-04-08 14:35:23 -0400

    fix compare function

--- generic_linear_search.c
+++ generic_linear_search.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int genericLinearSearch(void *array, size_t elemSize, int lenArray, void *key, int (*cmp)(void *a, void *b))
+// This generic linear search algorithms can search an array of with elements of any 
+// type.  It takes an array pointer, size of the elements, lenght of the array, the 
+// key, and a comparison function supplied by the user.  
+int genericLinearSearch(void *array, 
+                        size_t elemSize, 
+                        int lenArray, 
+                        void *key, 
+                        int (*cmp)(void *a, void *b))
 {
 
     for(int i = 0; i < lenArray; i++)
     {
+        // calculates location of current element
         void *currentElem = array + (elemSize * i);
+        //compares current element and key using fractionCompare function
         if (cmp(currentElem, key) == 0)
             return i;
     }
@@ -15,12 +23,14 @@ int genericLinearSearch(void *array, size_t elemSize, int lenArray, void *key, i
 
 }
 
+//create fraction struct
 typedef struct
 {
     int num, den;
 
 } fraction;
 
+// to compare fraction values, regardless of numerator and denominator sizes
 int fractionCompare(fraction *f1, fraction *f2)
 {
     float f1Val = (float)f1->num/(float)f1->den;
@@ -38,10 +48,12 @@ int main()
 
     fraction *key = (12,18);
 
+    //calculates length of array fractions.
     int arrayLength = sizeof(fractions)/sizeof(fraction);
 
     int indexOfKey = genericLinearSearch(fractions, sizeof(fraction), arrayLength, &key, fractionCompare);
 
+    //prints index of key to screen.  If key is not found, -1 will be printed to screen.  
     printf("%i\n", indexOfKey);
 
     return 0;
